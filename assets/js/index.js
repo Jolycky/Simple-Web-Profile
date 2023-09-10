f4dcfe00541dc57ef8e42d9064acc87751112e14
JoLycky <josebagus4@gmail.com>
2023-09-10 10:40:23 +0700

    index js

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+function main() {
+  var menuBtn = document.getElementById("menuBtn");
+  var sideNav = document.getElementById("sideNav");
+
+  sideNav.style.right = "-250px";
+
+  menuBtn.addEventListener("click", function () {
+    if (isOpen) {
+      sideNav.style.right = "-250px";
+    } else {
+      sideNav.style.right = "0";
+    }
+    isOpen = !isOpen;
+  });
+}
+
+window.onload = function () {
+  main();
+};
